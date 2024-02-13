83bcd71766d231e83a800a87959988fe09c222e4
Gabriel Alejo Cota <159669388+Gcota1694@users.noreply.github.com>
2024-02-13 13:43:54 -0800

    Add files via upload
    Ejecicio2 curso Udemy

---
+++ Ejercicio2.py
@@ -0,0 +1,10 @@
+num1= 23
+num2 = 54
+payaso= 112
+muneca= 75
+peso_totalp=payaso*num1
+peso_totalm=muneca*num2
+
+pesototalv = peso_totalp + peso_totalm
+
+print ('el peso total es de: ', pesototalv )
