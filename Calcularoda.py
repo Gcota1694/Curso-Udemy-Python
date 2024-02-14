78bdf20a813defb38939315b43baf6b40fb6dbab
Gabriel Alejo Cota <159669388+Gcota1694@users.noreply.github.com>
2024-02-14 12:32:44 -0800

    Add files via upload

---
+++ Calcularoda.py
@@ -0,0 +1,11 @@
+#calculadora basica 
+
+valor1=float,int(input('Ingresa el valor 1:'))
+valor2=float,int(input('Igresa el valor 2:'))
+
+print('esta es la suma de los valores: ',valor1+valor2)
+print('esta es la resta de los valores:',valor1-valor2)
+print('esta es la multipliccaiom de los valores',valor1*valor2)
+print('esta es la division de los valores:',valor1/valor2)
+print('esta es la divicion entera de los valores',valor1//valor2)
+print('este es el residuo de los valores',valor1%valor2)
